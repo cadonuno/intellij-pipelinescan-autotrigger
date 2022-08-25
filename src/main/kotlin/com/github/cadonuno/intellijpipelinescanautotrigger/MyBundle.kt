69a3ad15bba462017495c62b49967df78cec413c
GitHub Action <action@github.com>
2022-08-25 14:25:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/cadonuno/intellijpipelinescanautotrigger/MyBundle.kt
+++ src/main/kotlin/com/github/cadonuno/intellijpipelinescanautotrigger/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cadonuno.intellijpipelinescanautotrigger
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cadonuno/intellijpipelinescanautotrigger/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cadonuno/intellijpipelinescanautotrigger/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cadonuno.intellijpipelinescanautotrigger.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cadonuno.intellijpipelinescanautotrigger.MyBundle
 
 class MyApplicationService {
 
